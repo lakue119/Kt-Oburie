053b273454a8072da7a37f4a5633c13c8a5834ce
ChoiMyungRak <audfkr0808@naver.com>
2021-02-24 23:42:21 +0900

    아이콘 추가

--- app/src/main/java/com/lakue/oburie/ui/fragment/FragmentHome.kt
+++ app/src/main/java/com/lakue/oburie/ui/fragment/FragmentHome.kt
@@ -11,15 +11,8 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class FragmentHome : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -33,11 +26,6 @@ class FragmentHome : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentHome().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            FragmentHome()
     }
 }
