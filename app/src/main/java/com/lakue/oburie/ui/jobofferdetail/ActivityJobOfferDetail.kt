f69af9171f301e0252783086651e4086cec5c61f
ChoiMyungRak <audfkr0808@naver.com>
2021-03-23 22:58:10 +0900

    구해요 상세 레이아웃 구성

---
+++ app/src/main/java/com/lakue/oburie/ui/jobofferdetail/ActivityJobOfferDetail.kt
@@ -0,0 +1,12 @@
+package com.lakue.oburie.ui.jobofferdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lakue.oburie.R
+
+class ActivityJobOfferDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_offer_detail)
+    }
+}
