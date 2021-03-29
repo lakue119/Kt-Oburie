9c86e93a9c83cd05c2cb06c83eeedc74150efc54
ChoiMyungRak <audfkr0808@naver.com>
2021-03-29 22:40:47 +0900

    jobOffer layout 구성

--- app/src/main/java/com/lakue/oburie/ui/jobofferdetail/ActivityJobOfferDetail.kt
+++ app/src/main/java/com/lakue/oburie/ui/jobofferdetail/ActivityJobOfferDetail.kt
@@ -2,18 +2,26 @@ package com.lakue.oburie.ui.jobofferdetail
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Color
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lakue.oburie.R
 import com.lakue.oburie.base.BaseActivity
-import com.lakue.oburie.databinding.ActivityCategoryResultBinding
 import com.lakue.oburie.databinding.ActivityJobOfferDetailBinding
-import com.lakue.oburie.ui.categoryresult.CategoryResultActivity
-import com.lakue.oburie.ui.categoryresult.CategoryResultViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class ActivityJobOfferDetail  : BaseActivity<ActivityJobOfferDetailBinding, JobOfferDetailViewModel>(R.layout.activity_job_offer_detail) {
+class ActivityJobOfferDetail  : BaseActivity<ActivityJobOfferDetailBinding, JobOfferDetailViewModel>(R.layout.activity_job_offer_detail),
+    OnMapReadyCallback {
+
+    lateinit var mMap: GoogleMap
+
     companion object {
         fun startJobOfferDetailActivity(
                 context: Context
@@ -27,6 +35,9 @@ class ActivityJobOfferDetail  : BaseActivity<ActivityJobOfferDetailBinding, JobO
         binding.apply {
             vm = viewModel
         }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
     }
 
 
@@ -37,5 +48,26 @@ class ActivityJobOfferDetail  : BaseActivity<ActivityJobOfferDetailBinding, JobO
     }
 
     override fun setObserve() {
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+
+        // Add a marker in Sydney and move the camera
+        val location = LatLng(viewModel.liveJobOffer.value!!.lat, viewModel.liveJobOffer.value!!.lnt)
+        mMap.addMarker(MarkerOptions()
+            .position(location)
+            .title("공연위치"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f));
+
+        //거리 원 추가
+        var circleLength = CircleOptions().center(location)
+            .radius(500.0)   //반지름 = m
+            .strokeWidth(2f)    //선너비 0f = 선없음
+            .strokeColor(Color.parseColor("#0080FF"))
+            .fillColor(Color.parseColor("#4D72B9FF"))  //배경색
+
+        mMap.addCircle(circleLength)
+
     }
 }
