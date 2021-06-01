117cca7532c4e6102059e13e2bbf6d86b02ecd29
ChoiMyungRak <audfkr0808@naver.com>
2021-06-01 22:38:19 +0900

    CustomRecyclerView 생성

---
+++ app/src/main/java/com/lakue/oburie/view/ObRecyclerView.kt
@@ -0,0 +1,14 @@
+package com.lakue.oburie.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ObRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): RecyclerView(context, attrs, defStyleAttr) {
+
+}
